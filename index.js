859eb652c9121cea3cc5eb60bf4addcd6e5ce9f5
hotsuchi <suchitra38das@gmail.com>
2025-04-30 08:13:05 +0530

    ouhg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const path = require('path');
+const data = require(./data.json);
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,8 +9,8 @@ const port = process.env.PORT || 3000;
 app.get('/',(req,res)=>{
     res.send(path.join(__dirname,index.html));
 })
-app.get('/api',(req,res)=>{
-    res.json(path.join(__dirname,data.json));
+app.get('/dta',(req,res)=>{
+    res.json(data);
 })
 
 app.listen(port);
