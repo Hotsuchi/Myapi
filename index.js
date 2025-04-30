5905ab0551a1b036ce412a49a39ebbea13660b1d
hotsuchi <suchitra38das@gmail.com>
2025-04-30 08:04:04 +0530

    add json

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-let myObj={
-        'name':'tk',
-        'age':'34'
-    }
 
 app.get('/',(req,res)=>{
     res.send(path.join(__dirname,index.html));
 })
 app.get('/api',(req,res)=>{
-    res.json(myObj);
+    res.json(path.join(__dirname,data.json));
 })
 
 app.listen(port);
